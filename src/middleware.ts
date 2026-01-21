d39b4d19941451d49bd98cf53997612646ecf785
olhkyle <itskkyle@gmail.com>
2026-01-21 19:59:47 +0900

    fix : fix favicon problem

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,6 +16,8 @@ export async function middleware(request: NextRequest) {
 	if (!user) {
 		const redirectUrl = new URL(route.AUTH.LOGIN, request.url);
 
+		redirectUrl.searchParams.set('next', pathname);
+
 		return NextResponse.redirect(redirectUrl, {
 			headers: supabaseResponse.headers,
 		});
@@ -25,5 +27,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-	matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+	matcher: ['/((?!api|_next|.*\\..*).*)'],
 };
